2586e62c8d11a4060e269b0ee3e90a88cbb95bda
mmartinslovik <martinslovik1122@gmail.com>
2021-02-03 11:32:00 +0100

    Configured postgres db

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,10 +15,12 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         email = 'xxxxxd@HOVNO.COM'
-        user = get_user_model().objects.create_user(email=email, password='test123')
+        user = get_user_model().objects.create_user(
+                email=email,
+                password='test123'
+        )
 
         self.assertEqual(user.email, email.lower())
 
