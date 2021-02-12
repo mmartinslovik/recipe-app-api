4bf9883dd2458e3e2e17f83ede6421fb9403ff95
mmartinslovik <martinslovik1122@gmail.com>
2021-02-12 16:41:17 +0100

    added create ingredients feature

---
+++ app/recipe/tests/test_ingredients.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+class PublicIngredientsApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_reguired(self):
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+
+class PrivateIngredientApiTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email='majcher@jordialba.com',
+            password='hesloo',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_ingredients(self):
+        Ingredient.objects.create(user=self.user, name='sol')
+        Ingredient.objects.create(user=self.user, name='pepper')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        user2 =  get_user_model().objects.create_user(
+            email='bulostavy@jordialba.com',
+            password='hesloo',
+        )
+        Ingredient.objects.create(user=user2, name='ocet')
+        ingredient = Ingredient.objects.create(user=self.user, name='kumst')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    
